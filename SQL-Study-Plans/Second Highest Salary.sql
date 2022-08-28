40a21cc9dc9e9e86d41c4161f0f83d9e1e18d58b
Nitish k <91598274+Nitish36@users.noreply.github.com>
2022-08-28 18:20:49 +0530

    Create Second Highest Salary.sql

---
+++ SQL-Study-Plans/Second Highest Salary.sql
@@ -0,0 +1,48 @@
+Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+
+
+# Write your MySQL query statement below
+SELECT MAX(salary) as SecondHighestSalary
+FROM Employee
+WHERE salary NOT IN 
+(
+    SELECT MAX(salary) 
+    FROM Employee
+)
