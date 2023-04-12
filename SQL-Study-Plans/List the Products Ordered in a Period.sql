b1f645bae1ce26b3c5f750e5ddefffe3fe7a5c01
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-04-12 10:43:24 +0530

    Final Commit

---
+++ SQL-Study-Plans/List the Products Ordered in a Period.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT p.product_name,SUM(o.unit) AS unit
+FROM Products p,Orders o
+WHERE p.product_id = o.product_id 
+AND MONTH(o.order_date) = 2
+AND YEAR(o.order_date) = 2020
+GROUP BY p.product_name
+HAVING SUM(o.unit)>=100;
