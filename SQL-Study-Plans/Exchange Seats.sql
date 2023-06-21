260c0c5930bf4b1912fa31b03ff5bced16f9682a
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-06-21 12:48:07 +0530

    Final Commit

---
+++ SQL-Study-Plans/Exchange Seats.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+CASE WHEN s.id%2<>0 AND s.id = (SELECT COUNT(*) FROM Seat) THEN s.id 
+     WHEN s.id%2 = 0 THEN s.id-1
+     ELSE s.id+1
+    END AS id,
+     s.student
+FROM Seat as s
+Order BY id
