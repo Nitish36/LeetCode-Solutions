c1f926f7a227d7186a77dd5db1d0320a9b17c255
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-08-01 12:30:23 +0530

    Delete Patients With a Condition.py

--- 30-Days-Pandas/Patients With a Condition.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
-    condition = patients['conditions'].str.contains('DIAB1')
-    df = patients.loc[condition,['patient_id','patient_name','conditions']]
-
-    return df
