ede8307fa75b7346fb6e4360e723dace96cd4323
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2024-08-11 11:40:14 +0530

    Final Commit

--- Pandas/Drop Missing Data.py
+++ Pandas/Drop Missing Data.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
     df = pd.DataFrame(students,columns = ["student_id","name","age"])
-    df = df.dropna()
+    df = df.dropna(subset="name")
     return df
     
